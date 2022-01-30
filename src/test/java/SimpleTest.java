047e55af5d5e1f36086037c64a3dfe6658e5ba91
Александра <85274130+SashkaDikaya@users.noreply.github.com>
2022-01-30 16:44:30 +0300

    new commit (#1)
    added Test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void assertTrueTest() {
+
+        Assertions.assertTrue(3 > 2);
+    }
+}
